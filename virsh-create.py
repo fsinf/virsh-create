de8319bed2d22f8e2e78ed17e626cea0d7fb221c
Mathias Ertl <mati@er.tl>
2014-12-21 20:35:28 +0100

    fix ssh-keygen

--- virsh-create.py
+++ virsh-create.py
@@ -266,8 +266,10 @@ ex(['mv', 'etc/apt/sources.list.d/fsinf.list.backup', 'etc/apt/sources.list.d/fs
 # generate SSH key
 log.info('Generate SSH client keys for backups')
 ex(['rm', '-f', 'root/.ssh/id_*'])  # remove any prexisting SSH keys
-chroot(['ssh-keygen', '-q', '-N', '', '-f', '/root/.ssh/id_rsa', '-O', 'no-x11-forwarding',
-        '-O', 'source-address=%s,%s,%s,%s' % (ipv4, ipv6, ipv4_priv, ipv6_priv)])
+# Note: We force -t rsa, because we have to pass -f in order to be non-interactive
+chroot(['ssh-keygen', '-t', '-rsa', '-q', '-N', '', '-f', '/root/.ssh/id_rsa', '-O',
+        'no-x11-forwarding', '-O',
+        'source-address=%s,%s,%s,%s' % (ipv4, ipv6, ipv4_priv, ipv6_priv)])
 
 # fix hostname in public key:
 ex(['sed', '-i', 's/@[^@]*$/@%s/' % args.name, '/root/.ssh/id_rsa.pub'])
