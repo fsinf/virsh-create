3c36ba6b0c0b1f9d77a330a549f19d0ebb9f8e76
Mathias Ertl <mati@er.tl>
2017-07-19 13:38:59 +0200

    minor style updates

--- util/process.py
+++ util/process.py
@@ -3,18 +3,16 @@
 #
 # This file is part of virsh-create (https://github.com/fsinf/virsh-create).
 #
-# virsh-create is free software: you can redistribute it and/or modify it under the terms of the
-# GNU General Public License as published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
+# virsh-create is free software: you can redistribute it and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
 #
-# virsh-create is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
-# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
+# virsh-create is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+# for more details.
 #
-# You should have received a copy of the GNU General Public License along with virsh-create.  If
-# not, see <http://www.gnu.org/licenses/>.
-
-from __future__ import unicode_literals
+# You should have received a copy of the GNU General Public License along with virsh-create. If not, see
+# <http://www.gnu.org/licenses/>.
 
 import glob
 import logging
@@ -102,6 +100,7 @@ def mount(frm, lv_name, bootdisk, bootdisk_path):
         if not settings.DRY:
             log.debug('- rmdir %s', settings.CHROOT)
             os.removedirs(settings.CHROOT)
+
 
 def update_macs(mac, mac_priv):
     log.info("Update MAC addresses")
