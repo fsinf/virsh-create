5fc69098b8430b5c5fd4d1d269af33dcdf1d986d
Mathias Ertl <mati@er.tl>
2017-07-19 14:51:36 +0200

    update lvm module

--- util/lvm.py
+++ util/lvm.py
@@ -1,3 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# This file is part of virsh-create (https://github.com/fsinf/virsh-create).
+#
+# virsh-create is free software: you can redistribute it and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# virsh-create is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along with virsh-create. If not, see
+# <http://www.gnu.org/licenses/>.
+
 import logging
 
 from collections import namedtuple
@@ -12,13 +28,15 @@ log = logging.getLogger(__name__)
 def lvs():
     stdout, stderr = ex(['lvs', '--noheadings', '--separator', ';', '--units=b'], quiet=True,
                         dry=True)
-    return [LV(*line.strip().split(';')) for line in stdout.split()]
+    return [LV(*line.strip().split(';')) for line in stdout.decode('utf-8').split()]
+
 
 def lvdisplay(path):
     stdout, stderr = ex(['lvdisplay', '--noheadings', '--separator', ';', '--units=b', '-C', path],
                         quiet=True, dry=True)
-    return LV(*stdout.strip().split(';'))
+    return LV(*stdout.decode('utf-8').strip().split(';'))
+
 
 def lvcreate(vg, name, size):
     log.info('Create LV %s on VG %s', name, vg)
-    ex(['lvcreate', '-L', size, '-n', name, vg]),
+    ex(['lvcreate', '-L', size, '-n', name, vg])
